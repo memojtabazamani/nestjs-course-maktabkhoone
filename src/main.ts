056634194383a0958438797e9a58434eae244f01
Mojtaba zamani <memojtabazamani@gmail.com>
2026-03-16 14:56:34 +0330

    Init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const config = new DocumentBuilder().setTitle("Nest App").build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/documentation', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
