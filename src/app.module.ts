ff416416ffd6dfa7dc13ec71826a85d5b4a7f81e
Mojtaba zamani <memojtabazamani@gmail.com>
2026-03-16 20:42:05 +0330

    CRUD Blog Rest api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogController } from './blog/blog.controller';
+import { BlogService } from './blog/blog.service';
 
 // Decorator
 @Module({
   imports: [],
   controllers: [AppController, BlogController],
-  providers: [AppService],
+  providers: [AppService, BlogService],
 })
 export class AppModule {}
--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,40 +1,43 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { BlogDto } from './dtos/blog.dto';
+import { BlogService } from './blog.service';
 
 @ApiTags('Blog')
 @Controller('blog')
 export class BlogController {
+  constructor(private readonly blogService: BlogService) {}
   @Get()
   findAll(@Query() queryParams) {
-    return 'Find All Blogs';
-  }
-
-  @Get('category')
-  findAllCategories() {
-    return 'Find All Categories';
+    return this.blogService.findAll();
   }
 
   @Get(':id')
   findOn(@Param('id') id: string) {
-    return 'Category ID: ' + id;
+    return this.blogService.findOne(id);
   }
 
   @Post()
   create(@Body() body: BlogDto) {
-    console.log(body);
-    return 'Create Blog';
+    return this.blogService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body: BlogDto) {
-    console.log(id, body);
-    return 'Update Blog';
+    return this.blogService.update(id, body);
   }
 
   @Delete(':id')
   delete(@Param('id') id: string) {
-    console.log(id);
-    return 'Delete Blog';
+    return this.blogService.delete(id);
   }
 }
